240a228e936eba3a3af7ab4acf648a5376d57fb2
mariagrech <maria.grech.d57072@mcast.edu.mt>
2024-05-13 10:32:52 +0200

    Update Alarms.cs

--- Assets/Scripts/Alarms.cs
+++ Assets/Scripts/Alarms.cs
@@ -1,39 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UI;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Alarms : MonoBehaviour
 {
-    [SerializeField]
-    public Image image; 
-
     private bool alarm = false;
     private Color color1 = new Color(0.925f, 0.412f, 0.412f); // #EC6969
     private Color color2 = Color.red; // #FF0000
+    
 
-    void Start()
-    {
-        // Ensure the image component is correctly assigned
-        if (image == null)
-        {
-            image = GetComponent<Image>();
-            if (image == null)
-            {
-                Debug.LogError("Error: No Image component found on the GameObject.");
-                return;
-            }
-        }
-
-        alarm = true;
-
-        StartCoroutine(ChangeColor());
-    }
-
-    IEnumerator ChangeColor()
+    IEnumerator ChangeColor(Image image)
     {
         while (true)
         {
+            image = GetComponent<Image>();
             if (alarm)
             {
                 image.color = image.color == color1 ? color2 : color1;
@@ -50,6 +32,7 @@ public class Alarms : MonoBehaviour
     public void ToggleAlarm(Image img)
     {
         alarm = !alarm;
-        //StartCoroutine(ChangeColor(Image img));
+        
+        StartCoroutine(ChangeColor(img));
     }
 }
