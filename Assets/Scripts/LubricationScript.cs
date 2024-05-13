154840ca0307ea2615532af6c15070d54f8f4dc0
NoahMon <2013mnoah@gmail.com>
2024-05-13 11:25:10 +0200

    pish

--- Assets/Scripts/LubricationScript.cs
+++ Assets/Scripts/LubricationScript.cs
@@ -2,13 +2,15 @@ using Photon.Pun.Demo.PunBasics;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LubricationScript : MonoBehaviour
 {
     private GameManager _gameManager;
     private bool shoreOn;
-    [SerializeField]
-    private GameObject MeLo;
+    public Slider MeLoSlider;
+    public Slider DgLoSlider;
+    private bool check;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,12 +26,96 @@ public class LubricationScript : MonoBehaviour
         }
     }
 
-    public void onLubIntakeButtonPressOn()
+    public void MeLoIntakeButtonPressOn()
     {
         if (shoreOn)
         {
+            check = true;
             Debug.Log("Lub Tank ON");
-            
+            StopCoroutine(MeLoTankOff());
+            StartCoroutine(MeLoTankOn());
+        }
+    }
+
+    public void MeLoIntakeButtonPressOff()
+    {
+        if (shoreOn)
+        {
+            check = false;
+            Debug.Log("Lub Tank OFF");
+            StopCoroutine(MeLoTankOn());
+            StartCoroutine(MeLoTankOff());
+        }
+    }
+
+    public void DgLoButtonPressOn()
+    {
+        if (shoreOn)
+        {
+            check = true;
+            Debug.Log("DgLo Tank ON");
+            StopCoroutine(DgLoTankOff());
+            StartCoroutine(DgLoTankOn());
+        }
+    }
+
+    public void DgLoButtonPressOff()
+    {
+        if (shoreOn)
+        {
+            check= false;
+            Debug.Log("Lub Tank OFF");
+            StopCoroutine(DgLoTankOn());
+            StartCoroutine(DgLoTankOff());
+        }
+    }
+
+    public IEnumerator MeLoTankOn()
+    {
+        while (check)
+        {
+            MeLoSlider.value+=0.1f;
+            yield return new WaitForSeconds(1f);
+            if(MeLoSlider.value == MeLoSlider.maxValue)
+            {
+                check = false;
+            }
+        }
+    }    
+    public IEnumerator DgLoTankOn()
+    {
+        while (check)
+        {
+            DgLoSlider.value += 0.1f;
+            yield return new WaitForSeconds(1f);
+            if (DgLoSlider.value == DgLoSlider.maxValue)
+            {
+                check = false;
+            }
+        }
+    }    
+    public IEnumerator MeLoTankOff()
+    {
+        while (check == false)
+        {
+            MeLoSlider.value -= 0.1f;
+            yield return new WaitForSeconds(1f);
+            if (MeLoSlider.value == MeLoSlider.minValue)
+            {
+                check = true;
+            }
+        }
+    }    
+    public IEnumerator DgLoTankOff()
+    {
+        while (check == false)
+        {
+            DgLoSlider.value -= 0.1f;
+            yield return new WaitForSeconds(1f);
+            if (DgLoSlider.value == DgLoSlider.minValue)
+            {
+                check = true;
+            }
         }
     }
 }
