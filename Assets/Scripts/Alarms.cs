a88f72753d94106ae389a421d56daa59603bbd1f
Nah0ra <nah0ra@outlook.com>
2024-05-20 13:16:31 +0200

    - Cooling & Alarms

--- Assets/Scripts/Alarms.cs
+++ Assets/Scripts/Alarms.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEditor.UI;
 using UnityEngine;
 using UnityEngine.UI;
@@ -13,21 +14,29 @@ public class Alarms : MonoBehaviour
     private Color color1 = new Color(0.925f, 0.412f, 0.412f); // #EC6969
     private Color color2 = Color.red; // #FF0000
 
+    private GameManager _gameManager;
+
+    private PhotonView photonView;
+
     // private IEnumerator coroutine;
 
     private void Start()
     {
         _alarmImage.GetComponent<Image>();
+        _gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         //coroutine = ChangeColor();
     }
 
     private void Update()
     {
-        if (alarm) 
-            _alarmImage.color = _alarmImage.color == color1 ? color2 : color1;
-        else
+        if (_gameManager.AllClients)
         {
-            _alarmImage.color = color1;
+            if (alarm) 
+            _alarmImage.color = _alarmImage.color == color1 ? color2 : color1;
+            else
+            {
+                _alarmImage.color = color1;
+            }
         }
     }
 
